7a428200669e4677aa22ad57ca8f771833d1574d
Ignacioskm <naxomellado@gmail.com>
2026-03-25 17:36:24 -0300

    Se agrega base del proyecto - dependencias basicas

---
+++ VitalPetConnect/src/test/java/com/microservices/VitalPetConnect/VitalPetConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.VitalPetConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalPetConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
