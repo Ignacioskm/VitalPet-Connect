dbe2ef571153e8700582a7ab85b00eea76e092d7
Ignacioskm <naxomellado@gmail.com>
2026-05-06 20:10:43 -0400

    feat(ms-adoption): Impl controller and testing

--- vitalpet-connect/ms-adoptions/src/main/java/com/vitalpet/msadoptions/service/AdoptionService.java
+++ vitalpet-connect/ms-adoptions/src/main/java/com/vitalpet/msadoptions/service/AdoptionService.java
@@ -1,10 +1,10 @@
 package com.vitalpet.msadoptions.service;
 
 import com.vitalpet.msadoptions.client.PetClient;
-import com.vitalpet.msadoptions.client.StaffClient;
 import com.vitalpet.msadoptions.client.UserClient;
 import com.vitalpet.msadoptions.dto.AdoptionRequestDTO;
 import com.vitalpet.msadoptions.dto.AdoptionResponseDTO;
+import com.vitalpet.msadoptions.dto.PetResponseDTO;
 import com.vitalpet.msadoptions.model.Adoption;
 import com.vitalpet.msadoptions.model.AdoptionStatus;
 import com.vitalpet.msadoptions.repository.AdoptionRepository;
@@ -12,26 +12,81 @@ import com.vitalpet.msadoptions.repository.AdoptionStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class AdoptionService {
 
-    @Autowired
-    private AdoptionRepository adoptionRepository;
+    @Autowired  private AdoptionRepository adoptionRepository;
+    @Autowired  private AdoptionStatusRepository adoptionStatusRepository;
+    @Autowired  private PetClient petClient;
+    @Autowired  private UserClient userClient;
 
-    @Autowired
-    private AdoptionStatusRepository adoptionStatusRepository;
 
-    @Autowired
-    private PetClient petClient;
+    public AdoptionResponseDTO create(AdoptionRequestDTO dto) {
 
-    @Autowired
-    private UserClient userClient;
+        //Validar si existe pet y user
+        if (!petClient.existById(dto.getPetId())) throw new RuntimeException("La mascota no existe.");
+        if (!userClient.existById(dto.getUserId())) throw new RuntimeException("El usuario no existe.");
 
-    @Autowired
-    private StaffClient staffClient;
+        //Se asigna estado pending
+        AdoptionStatus adoptionStatus = adoptionStatusRepository.findByName("PENDING").orElseThrow(() -> new RuntimeException("Estado PENDING no encontrado."));
+
+        Adoption adoption = new Adoption();
+        adoption.setNotes(dto.getNotes());
+        adoption.setAdoptionStatus(adoptionStatus); //Se setea ya que por defecto el estado es PENDING
+        adoption.setPetId(dto.getPetId());
+        adoption.setUserId(dto.getUserId());
+        adoption.setRequestDate(LocalDateTime.now());
+
+        return toDTO(adoptionRepository.save(adoption));
+    }
+
+    //Trae todas las adopciones
+    public List<AdoptionResponseDTO> getAll() {
+        return adoptionRepository.findAll().stream().map(this::toDTO).toList();
+    }
+
+    //Trae mascotas disponibles para adopción
+    public List<PetResponseDTO> getAvailablePets() {
+        return  petClient.getAvailablePets();
+    }
+
+    //Aprobar adopción
+    public AdoptionResponseDTO approve(Long id, Long staffId){
+        Adoption adoption = adoptionRepository.findById(id).orElseThrow((() -> new RuntimeException("Adopción no encontrada")));
+
+        AdoptionStatus approved = adoptionStatusRepository.findByName("APPROVED").orElseThrow(() -> new RuntimeException("Estado APPROVED no existe"));
+
+        adoption.setAdoptionStatus(approved);
+        adoption.setStaffId(staffId);
+        adoption.setResolveDate(LocalDateTime.now());
+
+        Adoption saved = adoptionRepository.save(adoption);
+
+        petClient.updateOwner(adoption.getPetId(),adoption.getUserId());
+
+        return toDTO(saved);
+
+    }
+
+    //Rechazar
+    public AdoptionResponseDTO reject(Long id, Long staffId){
+
+        Adoption adoption = adoptionRepository.findById(id).orElseThrow(() -> new RuntimeException("Adopción no encontrada"));
+
+        AdoptionStatus rejected = adoptionStatusRepository.findByName("REJECTED").orElseThrow(() -> new RuntimeException("Estado REJECTED no existe"));
+
+        adoption.setAdoptionStatus(rejected);
+        adoption.setStaffId(staffId);
+        adoption.setResolveDate(LocalDateTime.now());
+
+        return toDTO(adoptionRepository.save(adoption));
+    }
+
+
+
 
     public AdoptionResponseDTO toDTO(Adoption adop) {
         AdoptionResponseDTO dto = new AdoptionResponseDTO();
@@ -46,43 +101,7 @@ public class AdoptionService {
         if (adop.getAdoptionStatus() != null) {
             dto.setStatusName(adop.getAdoptionStatus().getName()); //Agrega el nombre del status para mostrarlo en JSON
         }
-
         return dto;
     }
 
-    public AdoptionResponseDTO create(AdoptionRequestDTO dto) {
-
-        if (petClient.)
-
-        if (!petClient.existById(dto.getPetId())) throw new RuntimeException("La mascota no existe.");
-        if (!userClient.existById(dto.getUserId())) throw new RuntimeException("El usuario no existe.");
-        if (!userClient.isClient(dto.getUserId())) throw new RuntimeException("El usuario no es CLIENT.");
-        if (!staffClient.existById(dto.getStaffId())) throw new RuntimeException("El Staff no existe.");
-
-        AdoptionStatus adoptionStatus = adoptionStatusRepository.findByName("PENDING").orElseThrow(() -> new RuntimeException("Estado PENDING no encontrado."));
-
-        Adoption adoption = new Adoption();
-        adoption.setNotes(dto.getNotes());
-        adoption.setAdoptionStatus(adoptionStatus); //Se setea ya que por defecto el estado es PENDING
-        adoption.setPetId(dto.getPetId());
-        adoption.setUserId(dto.getUserId());
-        adoption.setStaffId(dto.getStaffId());
-
-        return toDTO(adoptionRepository.save(adoption));
-    }
-
-    //Trae todas las adopciones
-    public List<AdoptionResponseDTO> getAll() {
-        return adoptionRepository.findAll().stream().map(this::toDTO).collect(Collectors.toList());
-    }
-
-    //Trae adopciones por user
-    public List<AdoptionResponseDTO> getByUser(Long userId) {
-        return adoptionRepository.findByUserId(userId).stream().map(this::toDTO).toList();
-    }
-
-    //Trae mascotas disponibles para adopción
-    public List<Object> getAvailablePets() {
-        return  petClient.getAvailablePets();
-    }
 }
