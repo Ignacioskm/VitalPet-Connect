7722bfaf73d840fa7db28712d20944ffbfa4cdba
martimolsv <ma.molinas@duocuc.cl>
2026-05-06 22:41:14 -0400

    create-and-fix-validation-ms-fase-1

--- vitalpet-connect/ms-staff/src/main/java/com/vitalpet/msstaff/dto/ScheduleRequestDTO.java
+++ vitalpet-connect/ms-staff/src/main/java/com/vitalpet/msstaff/dto/ScheduleRequestDTO.java
@@ -1,5 +1,8 @@
 package com.vitalpet.msstaff.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,15 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ScheduleRequestDTO {
+
+    @NotNull(message = "El día de la semana no puede estar vacío.")
     private DayOfWeek dayOfWeek;
+
+    @NotNull(message = "El comienzo del turno no puede estar vacío.")
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime startTime;
+
+    @NotNull(message = "El término del turno no puede estar vacío.")
+    @JsonFormat(pattern = "HH:mm:ss")
     private LocalTime endTime;
 }
